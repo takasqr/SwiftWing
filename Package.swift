99ef5ee6e349c0d0bf749b5a80b5ff4578192080
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-06 14:28:32 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftWing",
-    products: [
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ], products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftWing",
