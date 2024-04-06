f58fb41bc971ffe4904ae168e6983b2b4e6b4850
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-06 13:13:49 +0900

    Create APIClient.swift

---
+++ Sources/SwiftWing/Network/APIClient.swift
@@ -0,0 +1,79 @@
+//
+//  APIClient.swift
+//
+//
+//  Created by takasqr on 2024/04/06.
+//
+
+import Foundation
+
+@available(macOS 12.0, *)
+class APIClient {
+    func get(urlString: String, headers: [String: String] = [:]) async throws -> (Data, URLResponse) {
+        let url: URL = URL(string: urlString)!
+        var request = URLRequest(url: url)
+
+        // Add headers to the request
+        for (key, value) in headers {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+
+        // 返却用パラメータを初期化
+        var returnData: Data = Data()
+        var returnResponse: HTTPURLResponse = HTTPURLResponse()
+
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+
+            guard let httpResponse = response as? HTTPURLResponse,
+                  httpResponse.statusCode == 200 else {
+                // return "Error..."
+                print("Error", response)
+                return (Data(), response)
+            }
+
+            returnData = data
+            returnResponse = httpResponse
+        } catch {
+            throw error
+        }
+
+        return (returnData, returnResponse)
+    }
+    
+    func post(urlString: String, headers: [String: String] = [:], body: Data) async throws -> (Data, URLResponse) {
+        guard let url = URL(string: urlString) else {
+            throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = body
+
+        // ヘッダーを追加
+        for (key, value) in headers {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+
+        // 返却用パラメータを初期化
+        var returnData: Data = Data()
+        var returnResponse: HTTPURLResponse = HTTPURLResponse()
+
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+
+            guard let httpResponse = response as? HTTPURLResponse,
+                  200...299 ~= httpResponse.statusCode else {
+                print("Error: \(response)")
+                return (Data(), response)
+            }
+
+            returnData = data
+            returnResponse = httpResponse
+        } catch {
+            throw error
+        }
+
+        return (returnData, returnResponse)
+    }
+}
