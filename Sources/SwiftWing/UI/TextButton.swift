45c57ae50787e81d5467771a2f9dc706cd74e4d6
takasqr <95556025+takasqr@users.noreply.github.com>
2024-04-06 14:52:15 +0900

    Update TextButton.swift

--- Sources/SwiftWing/UI/TextButton.swift
+++ Sources/SwiftWing/UI/TextButton.swift
@@ -8,12 +8,17 @@
 import Foundation
 import SwiftUI
 
-@available(macOS 12.0, *)
 public struct TextButton: View {
     
     let label: String
     let icon: String
     let color: Color
+    
+    public init(label: String, icon: String, color: Color) {
+        self.label = label
+        self.icon = icon
+        self.color = color
+    }
     
     init(_ label: String, _ icon: String, _ color: Color = Color(red: 47/255, green: 88/255, blue: 205/255)) {
         
